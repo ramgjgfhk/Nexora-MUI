4bef7bef2ff3e4edb1d50e43c57bf8b26dbb7cae
Chokkalingam P <lingam.s@linuxoft.com>
2025-06-20 19:57:33 +0530

    f

---
+++ pages/segment.js
@@ -0,0 +1,13 @@
+import Dashboard from "@/Views/Dashboard";
+import SegmentList from "@/Views/Segment/segmentlist";
+import React from "react";
+
+const SegmentPage = () => {
+  return (
+    
+      <SegmentList />
+
+  );
+};
+
+export default SegmentPage;
